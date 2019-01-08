83948628ae90e9a4848555a51cd892d9b8669f83
Lucca Lu <a7xfazz@mmm.com>
2019-01-08 09:14:01 +0800

    bak 20190108

---
+++ tjbigdata/tjbigdata/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for tjbigdata project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+
+import os
+from os.path import join, dirname, abspath
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+
+import sys
+sys.path.insert(0, PROJECT_DIR)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tjbigdata.settings')
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
